21c45e1406a209d7d671e4d6019cc51e60592113
Erik Drozina <erikdro41@gmail.com>
2021-10-12 10:09:07 +0200

    Switch to SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS
    warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    Signed-off-by: Erik Drozina <erikdro41@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,7 +162,7 @@ VENDOR_SECURITY_PATCH := 2021-05-01
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
