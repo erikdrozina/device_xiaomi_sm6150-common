317b0f017aa4acd3154a0b1a28412da890309cae
Zinadin Zidan <zidan.roking@gmail.com>
2022-01-03 15:31:24 +0100

    sm6150-common: Add a minimal framework compatibility matrix
    * Made with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,6 +89,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/hidl/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/hidl/compatibility_matrix.xml
 ODM_MANIFEST_FILES += $(COMMON_PATH)/configs/hidl/manifest-qva.xml
 
