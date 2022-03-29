7f3de7115fb330e235c51937afca176580f4a9c0
Chenyang Zhong <zhongcy95@gmail.com>
2022-03-29 23:00:08 +0200

    sm6150-common: Build media Codec2 modules

--- sm6150.mk
+++ sm6150.mk
@@ -110,6 +110,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Codec2 modules
+PRODUCT_PACKAGES += \
+    com.android.media.swcodec \
+    libsfplugin_ccodec
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
